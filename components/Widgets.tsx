6932d1a03572f259e8ced3369f4d454034388a41
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-05-10 14:48:14 -0300

    WidtgetAdjust

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,19 +1,33 @@
 import { SearchIcon } from '@heroicons/react/outline'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {TwitterTimelineEmbed} from "react-twitter-embed"
+import {useSession} from "next-auth/react"
 
 const Widgets = () => {
+
+  const [value, setValue] = useState<string>("")
+
+  const handleChange = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+    e.preventDefault()
+    setValue(document.getElementById("searchInput").value)
+    console.log(value)
+  }
+
   return (
       <div className="mt-2 px-2 col-span-2 hidden lg:inline">
         <div className="flex item-center space-x-2 bg-gray-100 p-3 rounded-full">
             <SearchIcon className="w-5 h-5"/>
-            <input type="text" placeholder="Search Twitter" className="flex-1 outline-none bg-transparent"/>
+            <input type="text" id="searchInput" placeholder="Search Twitter" className="flex-1 outline-none bg-transparent"/>
+        </div>
+        <div className="mt-2 flex flex-1 justify-center mb-2">
+          <input onClick={handleChange} className="bg-twitter rounded-full  cursor-pointer text-white p-2 flex " type="submit" value="Search"/>
         </div>
         <TwitterTimelineEmbed
             sourceType="profile"
-            screenName="sonnysangha"
+            screenName={value}
             options={{height: 1000}}
         />      
+
     </div>
 
 
